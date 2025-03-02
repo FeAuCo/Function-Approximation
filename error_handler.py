1635c44d4c495c285bf7fe3b67842dd55d6ccfd1
FeAuCo <vladikyudin09@gmail.com>
2025-03-03 00:11:12 +0300

    errors associated with function added

--- error_handler.py
+++ error_handler.py
@@ -1,10 +1,15 @@
 import visual
-import math
+from sympy import *
 
 
-#
-# def unexpected_input_func():
-#     return True
+def unexpected_input_func():
+    x = symbols('x')
+
+    try:
+        test_func = eval("".join(visual.settings_entry_func.get().split()))
+        return False
+    except NameError:
+        return True
 
 
 def unexpected_input_point():
@@ -17,7 +22,16 @@ def unexpected_input_point():
 
 
 def unexpected_input_n():
-        if visual.settings_entry_n.get().isdigit() and int(visual.settings_entry_n.get()) > 0:
-            return False
-        else:
-            return True
+    if visual.settings_entry_n.get().isdigit() and int(visual.settings_entry_n.get()) > 0:
+        return False
+    else:
+        return True
+
+
+def func_is_not_defined_at_point():
+    try:
+        test_func = (lambdify(symbols('x'), "".join(visual.settings_entry_func.get().split()))
+                     (float(eval(visual.settings_entry_point.get()))))
+        return False
+    except ZeroDivisionError:
+        return True
