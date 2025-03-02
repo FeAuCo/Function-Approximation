c4f470d081e5378b382764601039972cd648ae73
FeAuCo <vladikyudin09@gmail.com>
2025-03-02 14:38:12 +0300

    new feature and code optimized

--- approximation.py
+++ approximation.py
@@ -1,45 +1,43 @@
-import numpy as np
-from matplotlib import pyplot as plt
-import sympy as smp
+from sympy import *
 import math
 import error_handler
 import visual
 
 
-def approximate(func, point, n):
+def approximate(func, point, n, x):
     if n == 1:
-        return f'{func(point).evalf()} * x / x'
+        return f'{lambdify(x, func)(point)} * x / x'
 
-    arg = smp.symbols('arg', real=True)
     approximated_func = ''
 
     for i in range(0, n):
         if i == 0:
-            approximated_func += f'({func(point).evalf()}) + '
+            approximated_func += f'({lambdify(x, func)(point)}) + '
 
         else:
-            derivative = smp.diff(func(arg), (arg, i))
+            derivative = diff(eval(str(func)), x, i)
 
-            approximated_func += f'(({derivative.evalf(subs={arg: point})}) '
+            approximated_func += f'(({lambdify(x, derivative)(point)}) '
             approximated_func += f'* (x - {point}) ** {i}) / {math.factorial(i)} + '
 
-    return smp.Symbol(approximated_func[:-3])
+    return Symbol(approximated_func[:-3])
 
 
-def set_params_and_run(y_func, approximate_func):
+def set_params_and_run():
     if (not error_handler.unexpected_input_point() or not error_handler.unexpected_input_n()):
-        plt.rcParams["figure.figsize"] = (10, 7)
+        func = visual.settings_entry_func.get()
+        point = float(visual.settings_entry_point.get())
+        n = int(visual.settings_entry_n.get())
 
-        fig, ax = plt.subplots()
-        x = np.linspace(float(visual.settings_entry_point.get()) - 5, float(visual.settings_entry_point.get()) + 5, 100)
-        y = y_func(x)
+        x, y = symbols(f'x {"".join(func.split())}', real=True)
 
-        ax.grid()
-        ax.plot(x, y)
+        approximated = lambdify(x, approximate(y, point, n, x))
 
-        approximated = smp.lambdify(smp.Symbol('x', real=True),
-                                    approximate(approximate_func, float(visual.settings_entry_point.get()),
-                                                int(visual.settings_entry_n.get())))
+        graphs = plotting.plot(lambdify(x, func), (x, point - 5, point + 5),
+                               ylim=(lambdify(x, func)(point - 5), lambdify(x, func)(point + 5)),
+                               show=False)
+        graphs.append(plotting.plot(approximated, (x, point - 5, point + 5),
+                                    ylim=(lambdify(x, func)(point - 5), lambdify(x, func)(point + 5)),
+                                    show=False)[0])
 
-        ax.plot(x, approximated(x), "black")
-        plt.show()
+        graphs.show()
--- tech.py
+++ tech.py
@@ -1,10 +1,6 @@
 import visual
 import approximation
-import numpy as np
-import sympy as smp
 
-
-visual.appr_btn['command'] = lambda: (approximation.set_params_and_run(getattr(np, visual.settings_entry_func.get()),
-                                                                       getattr(smp, visual.settings_entry_func.get())))
+visual.appr_btn['command'] = approximation.set_params_and_run
 
 visual.window.mainloop()
